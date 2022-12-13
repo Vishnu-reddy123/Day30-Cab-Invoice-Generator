6c27e8639e673f91e368ce43d866d2fea470dd77
Vishnu-reddy123 <vishnu95532@gmail.com>
2022-12-13 19:59:30 +0530

    added logic to calculate premium ride fare for user

---
+++ src/main/java/org/example/CabInvoiceGenerator/InvoiceFactory.java
@@ -0,0 +1,10 @@
+package org.example.CabInvoiceGenerator;
+
+public class InvoiceFactory {
+
+    //Method for getting instance of invoice object
+    public static IInvoice getInvoiceInstance(Invoice.TypeOfSubscription typeOfSubscription) {
+        return new Invoice(typeOfSubscription);
+    }
+
+}
